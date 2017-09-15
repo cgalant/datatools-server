7affcd74c2df3012139f0b6ac6bb8bead33c211a
Landon Reed <landontreed@gmail.com>
2017-09-15 15:18:59 -0400

    entirely remove old mapdb DataStore

--- src/main/java/com/conveyal/datatools/manager/DataManager.java
+++ src/main/java/com/conveyal/datatools/manager/DataManager.java
@@ -289,7 +289,7 @@ public class DataManager {
             registerExternalResource(new TransitFeedsFeedResource());
         }
     }
-    private static void loadConfig (String[] args) throws IOException {
+    public static void loadConfig (String[] args) throws IOException {
         FileInputStream configStream;
         FileInputStream serverConfigStream;
 
